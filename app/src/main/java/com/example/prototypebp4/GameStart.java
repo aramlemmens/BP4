b38c4431c19633617f3fe16db6e4b7e20d2a1886
A. Lemmens <aram.lemmensvelden@gmail.com>
2022-06-01 14:11:02 +0200

    [add] android studio

---
+++ app/src/main/java/com/example/prototypebp4/GameStart.java
@@ -0,0 +1,13 @@
+package com.example.prototypebp4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_start);
+    }
+}
