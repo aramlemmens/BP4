8166b0f66cc24365e77b1973aa3a20d8a3d87b3e
aram lemmens <aram.lemmensvelden@gmail.com>
2022-06-03 12:45:49 +0200

    [Add] game

--- app/src/main/java/com/example/prototypebp4/GameStart.java
+++ app/src/main/java/com/example/prototypebp4/GameStart.java
@@ -1,7 +1,12 @@
 package com.example.prototypebp4;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class GameStart extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class GameStart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_start);
+
+        ImageButton alleen = findViewById(R.id.imageButton_alleen);
+        alleen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(v.getContext(),Game.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton samen = findViewById(R.id.imageButton2_samen);
+        samen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(v.getContext(),Game.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
